3ed4cb30e10e510e4cd9b33cd9b11cda170859c2
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2025-11-28 23:29:03 +0900

    #411

--- scripts/rp.py
+++ scripts/rp.py
@@ -550,7 +550,7 @@ class Script(modules.scripts.Script):
 
         # SBM ddim / plms detection.
         self.isvanilla = p.sampler_name in ["DDIM", "PLMS", "UniPC"]
-        if forge:
+        if forge or reforge:
             self.isvanilla = not self.isvanilla
 
         if self.h % ATTNSCALE != 0 or self.w % ATTNSCALE != 0:
